cc1506a1c25d91dabb12c5bdcce9016e8393b59e
Wahyu Saefulloh <saefuuloh@gmail.com>
2022-07-06 11:37:20 +0700

    add

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,9 @@
-/*!
+import React, { Component } from "react";
+import Router from "next/router";
 
-=========================================================
-* NextJS Argon Dashboard PRO - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-argon-dashboard-pro
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
-import React from "react";
-import Link from "next/link";
-// reactstrap components
-import {
-  Badge,
-  Button,
-  Card,
-  CardBody,
-  Container,
-  Row,
-  Col,
-  UncontrolledTooltip,
-} from "reactstrap";
-// core components
-import IndexNavbar from "components/Navbars/IndexNavbar.js";
-import IndexHeader from "components/Headers/IndexHeader.js";
-import AuthFooter from "components/Footers/AuthFooter.js";
-
-function Index() {
+export default function Error404() {
   React.useEffect(() => {
     Router.push("/homepage");
   });
   return <div />;
 }
-
-export default Index;
